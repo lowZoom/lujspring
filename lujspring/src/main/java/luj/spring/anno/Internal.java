65c05efc55267d4687ad66a99a3d595d682a42af
luj <david_lu_st@163.com>
2022-11-23 21:55:05 +0800

    调整注解位置

--- lujspring/src/main/java/luj/spring/anno/Internal.java
+++ lujspring/src/main/java/luj/spring/anno/Internal.java
@@ -1,16 +1,12 @@
-package luj.ava.spring;
+package luj.spring.anno;
 
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-/**
- * @see luj.spring.anno.Internal
- */
-@Deprecated
-@Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
 public @interface Internal {
   // NOOP
 }
